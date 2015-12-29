a76b7869b7a2cbf33105b7e3524fa258ceabd75d
powerboat9 <powerboat9@users.noreply.github.com>
2015-12-29 17:48:23 -0500

    Worked on it a bit.

--- AI/getPos.lua
+++ AI/getPos.lua
@@ -1,1 +1,28 @@
-os.loadAPI("AUTONOMUS/AI/data/pos")
+local
+
+if fs.exists("AUTONOMUS/AI/data/pos") then
+    os.loadAPI("AUTONOMUS/AI/data/pos")
+else
+    pos = {
+        x = 0,
+        y = 0,
+        z = 0,
+        direction = 1
+    }
+end
+
+oldUp = turtle.up
+oldDown = turtle.down
+oldForward = turtle.forward
+oldBack = turtle.back
+oldLeft = turtle.turnLeft
+oldRight = turtle.turnRight
+
+local update = function()
+    pos
+
+turtle.up = function()
+    oldUp()
+    pos.y = pos.y + 1
+end
+
